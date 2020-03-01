76e0468e2319262cc9cb33e4a19f3f9a8b11201a
ilan M <ilanm@ilans-iMac.local>
2020-03-01 13:11:37 +0530

    player rank

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getCarsAtEnd();
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -89,8 +89,10 @@ class Game {
       player.update();
     }
 
-    if(player.distance > 6000){
+    if(player.distance > 5250){
       gameState = 2;
+      player.rank += 1;
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -98,5 +100,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
